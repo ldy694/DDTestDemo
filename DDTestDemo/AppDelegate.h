0ca886445f7c7411a34b081784b9b7a8692b7f2a
ldy694 <linxingdong@linxingdongdemacbook-pro.local>
2016-06-29 16:46:46 +0800

    Initial Commit

---
+++ DDTestDemo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  DDTestDemo
+//
+//  Created by 林兴栋 on 16/6/29.
+//  Copyright © 2016年 林兴栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ DDTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDTestDemo
+//
+//  Created by 林兴栋 on 16/6/29.
+//  Copyright © 2016年 林兴栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
